2adb565a84da4eb20dc42204c4beba16125d6d1b
Blair Ciolk <72766555+blairwitchstudios@users.noreply.github.com>
2022-08-01 14:46:25 -0400

    Path Update
    -Added more paths

--- blairwitch.py
+++ blairwitch.py
@@ -1,6 +1,8 @@
+import time
+import sys
+
 print(" This was a creation made by BlairWitchStudios on 8/1/2022 at 3:58am")
-
-
+time.sleep(2)
 
 
 name = input("Please enter your name:")
@@ -10,10 +12,11 @@ print("Welcome to the woods,", name, ", be careful...")
 
 itemlist = ["flashlight", "switchblade", "compass"]
 usermainitem = ("Please choose an object to help you through the forest:")
-
+time.sleep(2)
 
 
 print(usermainitem)
+time.sleep(2)
 print(itemlist)
 
 usermainitem = input("Selected Item: ")
@@ -31,12 +34,13 @@ if usermainitem == "compass":
 
 path = ("left", "center", "right")
 
-leftpath = "You decide the left path is the best option and begin walking"
-centerpath = "You realize the center path is the best option and begin walking"
-rightpath = "You guess the right path is the best option and begin walking"
+leftpath = "You decide the left path is the best option and begin walking."
+centerpath = "You realize the center path is the best option and begin walking."
+rightpath = "You guess that the right path is the best option and begin walking."
 
+time.sleep(2)
 print("It's time to enter the woods. Which path shall you take?")
-
+time.sleep(2)
 print(path)
 
 userpath = input("Seleted Path: ")
@@ -48,8 +52,19 @@ if userpath == "center":
 if userpath == "right":
     print(rightpath)
     
-    
-path = ("turn back", "investigate", "continue forward")
+time.sleep(2)    
+userpath = ("turn back", "investigate", "continue forward")
 print("A snap of a twig makes you stop in your tracks.")
-print("What do you do?", path)    
+time.sleep(2)
+print("What do you do?")
+print(userpath)
+userpath = input("Selected option:")
+
+if userpath == "continue forward":
+    print("You decide it's more of a danger to turn back now. You continue forward...")
     
+else:
+    
+    sys.exit()
+
+
