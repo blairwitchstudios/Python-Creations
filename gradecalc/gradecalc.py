bcce5b7134bd6638725e3856d809f7242b3934db
Blair Ciolk <72766555+blairwitchstudios@users.noreply.github.com>
2022-08-01 15:24:30 -0400

    Creation
    -Create GradeCalc
    -Fucked around in GradeCalc
    -Realized I have to go to work

---
+++ gradecalc/gradecalc.py
@@ -0,0 +1,24 @@
+import time
+
+
+A1 = ("97", "98", "99", "100")
+A2 = ("93", "94", "95", "96")
+A3 = ("90", "91", "92")
+B1 = ("87", "88", "89")
+B2 = ("83", "84", "85","86")
+B3 = ("80", "81", "82")
+C1 = ("77", "78", "79")
+C2 = ("73", "74", "75", "76")
+C3 = ("70", "71", "72")
+D1 = ("67", "68", "69")
+D2 = ("63", "64", "65", "66")
+D3 = ("60", "61", "61")
+
+
+usergradepercentage = input("Enter current grade percent: ")
+
+print("Current grade percent: ", usergradepercentage, "%")
+print("Calculating grade...")
+
+time.sleep(1)
+
