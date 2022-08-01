f7dee21d5e8907d72e2b05c7df7945ccdfe547b5
Blair Ciolk <72766555+blairwitchstudios@users.noreply.github.com>
2022-08-01 12:42:49 -0400

    README update, General improvements
    -Edited the readme to be more accurate
    
    -Fixed grammatical errors
    
    -Continued work on "Forest"

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# Python Creations
- This is where I'll keep my python projects
+# Python Creations by BlairWitchStudios
+
+# This "Python Creations" repository was created to house the creations in Python
+# made by BlairWitchStudios.
+
+# There has been no set plan for any projects in this repository, as there should
+# never be expected dates for releases
--- blairwitch.py
+++ blairwitch.py
@@ -1,3 +1,8 @@
+print(" This was a creation made by BlairWitchStudios on 8/1/2022 at 3:58am")
+
+
+
+
 name = input("Please enter your name:")
 print("Welcome to the woods,", name, ", be careful...")
 
@@ -28,7 +33,7 @@ path = ("left", "center", "right")
 
 leftpath = "You decide the left path is the best option and begin walking"
 centerpath = "You realize the center path is the best option and begin walking"
-rightpath = "You guess the right path is the best option and begin wlaking"
+rightpath = "You guess the right path is the best option and begin walking"
 
 print("It's time to enter the woods. Which path shall you take?")
 
@@ -42,4 +47,9 @@ if userpath == "center":
     print(centerpath)
 if userpath == "right":
     print(rightpath)
+    
+    
+path = ("turn back", "investigate", "continue forward")
+print("A snap of a twig makes you stop in your tracks.")
+print("What do you do?", path)    
     
